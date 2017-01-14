2893939075536540a5ed0ea4f2b2e831b281068e
David McVicar <dwmcvicar@gmail.com>
2017-01-14 17:26:30 -0500

    added pug and mongoose to package.json, added sample templating code

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var express = require('express')
+var pug = require('pug')
+var mongoose = require('mongoose')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('<h1>THIS IS A TEST</h1>')
-})
+app.set('views','/Users/davidmcvicar/riding_right_site/web');
+app.set('view engine', 'pug');
+//app.use(express.static('web'))
+
+app.route('/blog')
+  .get(function (req, res) {
+    id = req.query.id
+    // get the blog post based on the id, from Mongo
+    message = 'You\'re getting blog post with the id: ' + id
+    res.render('index', { message: message });
+  })
+  .post(function (req, res) {
+    res.send(); 
+  });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
